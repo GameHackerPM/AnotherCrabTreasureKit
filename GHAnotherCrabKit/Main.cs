6f630e9b1a094c8efd7f89a3c45aaf0c90a7a782
GameHackerPM <GameHackerPM@hotmail.com>
2024-05-17 14:34:29 +0300

    Using game notification cause why not?

--- GHAnotherCrabKit/Main.cs
+++ GHAnotherCrabKit/Main.cs
@@ -23,20 +23,22 @@ namespace GHAnotherCrabKit
         public void Update()
         {
             _player = FindObjectOfType<Player>();
-            if (_player == null)
-                return;
-            if (Input.GetKeyDown(KeyCode.P))
+            if (_player != null)
             {
-                LastSavedPosition = _player.transform.position;
-                FadeLabel($"Location Saved... {LastSavedPosition.x}, {LastSavedPosition.y}, {LastSavedPosition.z}");
-            }
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    LastSavedPosition = _player.transform.position;
+                    FadeLabel($"{LastSavedPosition.x},{LastSavedPosition.y},{LastSavedPosition.z}");
+                    GUIManager.instance.HUD.itemNotification.Play("Location Saved.", true, null, "", 1);
+                }
 
-            if (Input.GetKeyDown(KeyCode.O) && LastSavedPosition != Vector3.zero)
-            {
-                _player.transform.position = LastSavedPosition;
-               FadeLabel($"Location Loaded... {LastSavedPosition.x}, {LastSavedPosition.y}, {LastSavedPosition.z}");
+                if (Input.GetKeyDown(KeyCode.O) && LastSavedPosition != Vector3.zero)
+                {
+                    _player.transform.position = LastSavedPosition;
+                    FadeLabel($"{LastSavedPosition.x},{LastSavedPosition.y},{LastSavedPosition.z}");
+                    GUIManager.instance.HUD.itemNotification.Play("Location Loaded.", true, null, "", 1);
+                }
             }
-
             if (Input.GetKeyDown(KeyCode.Delete))
             {
                 Loader.UnLoad();
@@ -46,19 +48,19 @@ namespace GHAnotherCrabKit
         public void FadeLabel(string message)
         {
             alphaAmount = 0f;
-            fadingLabelContent = $"Location Loaded... {LastSavedPosition.x}, {LastSavedPosition.y}, {LastSavedPosition.z}";
+            fadingLabelContent = message;
             showFadingLabel = true;
         }
 
         public void OnGUI()
         {
-            if(showFadingLabel && alphaAmount < 1f)
+            if (showFadingLabel && alphaAmount < 1f)
             {
                 alphaAmount += 0.3f * Time.deltaTime;
-                GUI.color = new Color(originalColor.r,originalColor.g,originalColor.b,alphaAmount);
-                GUI.Label(new Rect(Screen.width / 2, 40, 150, 50f), fadingLabelContent);
+                GUI.color = new Color(originalColor.r, originalColor.g, originalColor.b, alphaAmount);
+                GUI.Label(new Rect(Screen.width / 2, 40, 200f, 50f), fadingLabelContent);
             }
-            else if(alphaAmount >= 1f)
+            else if (alphaAmount >= 1f)
             {
                 alphaAmount = 0f;
                 GUI.color = originalColor;
