88b17f4ac213dd77a8de4cf9385e0ae461ef3359
GameHackerPM <GameHackerPM@hotmail.com>
2024-05-20 00:36:42 +0300

    Update version 0.5
    Update V0.5:
    Menu ('Insert' to show/hide the menu)
    Spawn and Equip all Shells types.
    Air Jump. (Infinite Jump)
    Invincible. (Can't be Hit)
    Invisible. (Stealth Mode, Enemy can't see you)

--- GHAnotherCrabKit/Main.cs
+++ GHAnotherCrabKit/Main.cs
@@ -11,18 +11,34 @@ namespace GHAnotherCrabKit
     {
         private Player _player;
         private Vector3 LastSavedPosition = Vector3.zero;
+        private List<ShellCollectable> ShellCollectableList = null;
         float alphaAmount = 0f;
         bool showFadingLabel = false;
         Color originalColor;
         string fadingLabelContent = "";
+        private bool isInvincible = false, airJump = false, isInvisible = false, invisiblityStatus = false;
+        private Rect windowRect = new Rect(0, 0, 400, 400);
+        private int tabIndex = 0;
+        private Color backgroundColor = Color.grey;
+        private bool showMenu = true;
+        private Vector2 mainScrollPos = Vector2.zero;
+        private Vector2 scrollPos = Vector2.zero;
         public void Start()
         {
-            //_player = FindObjectOfType<Player>();
+            ShellCollectableList = new List<ShellCollectable>();
+            foreach (var collectableItemData in InventoryMasterList.staticList)
+            {
+                if (collectableItemData is ShellCollectable shellCollectableItem)
+                {
+                    ShellCollectableList.Add(shellCollectableItem);
+                }
+            }
             FadeLabel("Game Injected!");
         }
         public void Update()
         {
-            _player = FindObjectOfType<Player>();
+            _player = Player.singlePlayer;
+            var gameManager = GameManager.instance;
             if (_player != null)
             {
                 if (Input.GetKeyDown(KeyCode.P))
@@ -38,11 +54,50 @@ namespace GHAnotherCrabKit
                     FadeLabel($"{LastSavedPosition.x},{LastSavedPosition.y},{LastSavedPosition.z}");
                     GUIManager.instance.HUD.itemNotification.Play("Location Loaded.", true, null, "", 1);
                 }
+                if (gameManager != null)
+                {
+                    if (Input.GetKeyDown(KeyCode.I))
+                    {
+                        gameManager.ToggleGodmode();
+                        DebugSettings_GodMode.godmode.AddDefaultGodMode();
+                        DebugSettings_GodMode.godmode.CycleGodMode();
+                    }
+                    if (Input.GetKeyDown(KeyCode.U))
+                    {
+                        isInvincible = !isInvincible;
+                        if (!isInvincible)
+                            _player.CancelInvincibility();
+
+                        // Physics.gravity = Vector3.down * 1f; // Gravity Modifier
+                    }
+                }
+
+                if (isInvincible)
+                    _player.MakeInvincible(float.MaxValue);
+
+                if (airJump)
+                {
+                    if (_player.input.controller.GetButtonDown("Jump"))
+                        _player.EjectJump();
+                }
+
+                if (isInvisible)
+                {
+                    invisiblityStatus = true;
+                    _player.statusEffects.stealth = true;
+                }
+                else if (invisiblityStatus)
+                {
+                    _player.statusEffects.stealth = false;
+                    invisiblityStatus = false;
+                }
             }
+
+            if (Input.GetKeyDown(KeyCode.Insert))
+                showMenu = !showMenu;
+
             if (Input.GetKeyDown(KeyCode.Delete))
-            {
                 Loader.UnLoad();
-            }
         }
 
         public void FadeLabel(string message)
@@ -51,7 +106,6 @@ namespace GHAnotherCrabKit
             fadingLabelContent = message;
             showFadingLabel = true;
         }
-
         public void OnGUI()
         {
             if (showFadingLabel && alphaAmount < 1f)
@@ -66,7 +120,63 @@ namespace GHAnotherCrabKit
                 GUI.color = originalColor;
                 showFadingLabel = false;
             }
+
+            if (showMenu)
+            {
+                GUI.backgroundColor = backgroundColor;
+
+                windowRect = GUI.Window(0, windowRect, MenuWindow, "Menu SDK <GH> V0.5 ('Insert' to Show/Hide Menu)");
+            }
             //GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 150f, 50f), "Game Injected!");
+        }
+        void MenuWindow(int windowID)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical(GUILayout.Width(100));
+            if (GUILayout.Toggle(tabIndex == 0, "Main", "Button", GUILayout.ExpandWidth(true)))
+            {
+                tabIndex = 0;
+            }
+
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+
+            switch (tabIndex)
+            {
+                case 0:
+                    GUILayout.BeginVertical(GUI.skin.box);
+                    mainScrollPos = GUILayout.BeginScrollView(mainScrollPos);
+                    GUILayout.Label("Equip Shells:");
+                    scrollPos = GUILayout.BeginScrollView(scrollPos, false, true, GUILayout.MaxHeight(120));
+                    foreach (var shellCollectable in ShellCollectableList)
+                    {
+                        if (GUILayout.Button(shellCollectable.name.Replace("Inv_Shell_", ""), GUILayout.MaxWidth(240)))
+                        {
+                            if (_player != null)
+                                shellCollectable.Equip();
+                        }
+                    }
+                    GUILayout.EndScrollView();
+                    GUILayout.Space(3);
+                    GUILayout.BeginHorizontal();
+                    GUILayout.BeginVertical();
+                    airJump = GUILayout.Toggle(airJump, "Air Jump");
+                    isInvincible = GUILayout.Toggle(isInvincible, "Invincible (U)");
+                    isInvisible = GUILayout.Toggle(isInvisible, "Invisible");
+                    GUILayout.Label("Key 'P' ==> Save Current Location.");
+                    GUILayout.Label("Key 'O' ==> Load Last Saved Location.");
+                    GUILayout.Label("Key 'Delete' ==> Unload the tool.");
+                    GUILayout.EndHorizontal();
+                    GUILayout.EndVertical();
+                    GUILayout.EndScrollView();
+                    GUILayout.EndVertical();
+                    break;
+            }
+
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+            GUI.DragWindow(); // Allow the user to drag the window around
         }
     }
 }
